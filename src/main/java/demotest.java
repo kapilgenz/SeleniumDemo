7358f81c5e599c262a2fc8f2bda2208f76ac4f54
kapilgenz <kapil.kushwah@genzsoln.com>
2024-07-16 15:10:41 +0530

    Updload project on git

---
+++ src/main/java/demotest.java
@@ -0,0 +1,23 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+public class demotest {
+ public static WebDriver driver ;
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		ChromeOptions options = new ChromeOptions();
+		options.addArguments("--ignore-certificate-errors");
+		options.addArguments("--start-maximized");
+		options.addArguments("--disable-gpu");
+		
+		 driver = new ChromeDriver(options);
+		 
+		 driver.get("https://community2.cloud-2.automationanywhere.digital/#/login");
+		 
+
+	}
+
+}
